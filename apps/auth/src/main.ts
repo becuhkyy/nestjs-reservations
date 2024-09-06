4694af37a6f1487cbbaa95e07f983a9f12a3813c
Vesko Vasilev <payvesipal@gmail.com>
2024-09-06 12:14:04 +0300

    fix config service in main

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
@@ -11,7 +12,7 @@ async function bootstrap() {
     }),
   );
   app.useLogger(app.get(Logger));
-  const configService = app.get('ConfigService');
+  const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { Logger } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
@@ -11,7 +12,7 @@ async function bootstrap() {
     }),
   );
   app.useLogger(app.get(Logger));
-  const configService = app.get('ConfigService');
+  const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
