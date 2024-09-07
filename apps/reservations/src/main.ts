9d3b6a32b0dde415ecb7a7a349177edcbc1be3af
Vesko Vasilev <payvesipal@gmail.com>
2024-09-07 23:59:25 +0300

    use cookie parser

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
